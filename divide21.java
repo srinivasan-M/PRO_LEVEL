54810e368a4ea2c9bb9c048a29e59185b5a08481
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 23:39:10 +0530

    Update divide21.java

--- divide21.java
+++ divide21.java
@@ -2,18 +2,21 @@ class divide21
 {
   public static void main(String args[])
   {
+	int x=0;
     int sum1=0,sum2=0;
     int[] a=new int[args.length/2];
     int[] b=new int[args.length/2];
   for(int i=0;i<args.length/2;i++)
   {
-    a[i]=Integer.valueOf(args[i]);
+    a[i]=Integer.valueOf(args[x]);
     sum1=sum1+a[i];
+	x++;
   }
-    for(int j=args.length/2;j<args.length;j++)
+    for(int j=0;j<args.length/2;j++)
   {
-    b[j]=Integer.valueOf(args[j]);
-    sum2=sum2+a[j];
+    b[j]=Integer.valueOf(args[x]);
+    sum2=sum2+b[j];
+	x++;
   }
     if(sum1==sum2)
     {
@@ -21,6 +24,7 @@ class divide21
       {
         System.out.print(a[k]+" ");
       }
+	   System.out.print("\n");
       for(int l=0;l<args.length/2;l++)
       {
         System.out.print(b[l]+" ");
