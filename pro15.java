9ac60fedf0dc66a94a8ee5c7c79e29f4497ab11c
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-05 17:33:52 +0530

    Update pro15.java

--- pro15.java
+++ pro15.java
@@ -1,1 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class pro15{
+
+	public static void main(String args[]) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int[] a=new int[n];
+		int[] b=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+			String s=Integer.toBinaryString(a[i]);
+			b[i]=s.length()-s.replaceAll("1","").length();
+		}
+		int[] c=new int[n];
+		System.arraycopy(b,0,c,0,b.length);
+		Arrays.sort(b);
+		for(int j=n-1;j>=0;j--)
+		{
+			for(int k=0;k<n;k++)
+			{
+				if(b[j]==c[k])
+				{
+					System.out.print(a[k]+" ");
+					break;
+				}
+			}
+		}
+	}
+}
+
+
+
+
 
